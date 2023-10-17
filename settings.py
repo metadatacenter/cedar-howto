7af7a6e9b67b555221a03410c117e38b8a4ad11c
Attila L. Egyedi <egyedia@stanford.edu>
2023-10-16 17:46:30 -0700

    Comment settings

--- settings.py
+++ settings.py
@@ -3,6 +3,7 @@ import os
 CEDAR_HOST = os.environ.get('CEDAR_HOST')
 API_KEY = os.environ.get('CEDAR_TEST_USER_API_KEY')
 
+# These are random dev values, replace them with your proper values from your system
 API_KEY = 'd88bb4a0ff66455f858cb2d5015b08def93483f1eb84217bdc141cabb56f12ee'
 SOURCE_FOLDER_ID = 'https://repo.metadatacenter.orgx/folders/f2ba6038-f596-4eb0-8519-ae13bd3b4903'
 TARGET_FOLDER_ID = 'https://repo.metadatacenter.orgx/folders/07f263cd-f2b8-4cb4-8fa7-385fcfb6545c'
